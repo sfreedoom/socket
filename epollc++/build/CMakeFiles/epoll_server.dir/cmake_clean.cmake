2be3b1330ee6294bc4be6c7fd6e36572f24fb93f
KIRA_CJ <1033956371@qq.com>
2020-04-15 23:22:53 +0800

    [task] add epoll c++ demo

---
+++ epollc++/build/CMakeFiles/epoll_server.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/epoll_server.dir/src/main.cpp.o"
+  "/bin/epoll_server.pdb"
+  "/bin/epoll_server"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/epoll_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ epollc++/build/CMakeFiles/socket.bin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/socket.bin.dir/src/main.cpp.o"
+  "/bin/socket.bin.pdb"
+  "/bin/socket.bin"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/socket.bin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
